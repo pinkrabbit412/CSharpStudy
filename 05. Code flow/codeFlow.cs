ebb430207f8f2831cb9e771b7f75a959e39af4b5
악동분홍토끼 <pinkrabbit412@daum.net>
2022-02-19 20:01:28 +0900

    Updated Chapter #5

--- 05. Code flow/codeFlow.cs
+++ 05. Code flow/codeFlow.cs
@@ -36,18 +36,50 @@ namespace CSharpStudy {
 
             // #3. while()
             int temp3 = 1;
-            while (temp3 <= 5) {
+            Console.Write("\n\n[?] while(): ");
+            while(++temp3 <= 5)
                 Console.Write(temp3);
-                temp3++;
-            }
+            Console.Write("\n[?] do while(): ");
             temp3 = 1;
             do {
                 Console.Write(temp3);
-                temp3++;
-            } while (temp3 <= 5);
+            } while(++temp3 <= 5);
 
-            // Program exit
-        end_of_program:
+            // #4. for()
+            Console.Write("\n[?] for(): ");
+            for (int temp4 = 1; temp4 <= 5; temp4++)
+                Console.Write(temp4);
+
+            // #5. foreach()
+            int[] temp5 = new int[]{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Console.Write("\n[?] int[] temp5 with for(): ");
+            foreach(int temp6 in temp5)
+                Console.Write(temp6);
+
+            // #6. continue
+            Console.Write("\n[?] int[] temp5 with for(), only odd-numbers: ");
+            foreach (int temp6 in temp5) {
+                if (temp6 % 2 == 0)
+                    continue;
+                Console.Write(temp6);
+            }
+
+            // #7. try, catch(), throw, break
+            Console.Write("\n[?] int[] temp5 with for(): ");
+            for (int temp7 = 0;; temp7++) {
+                try {
+                    if (temp7 < 0 || temp7 > temp5.Length)
+                        throw new IndexOutOfRangeException();
+                    Console.Write(temp5[temp7]);
+                }
+                catch(IndexOutOfRangeException error) {
+                    Console.WriteLine("\n[!] " + error);
+                    break;
+                }
+            }
+
+            // Program exit & return (End function and return some value)
+            end_of_program:
             Console.WriteLine("\n\n[?] Press any key to exit... ");
             Console.ReadKey();
             return;
