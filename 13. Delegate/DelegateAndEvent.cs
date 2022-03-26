4b5c74bc549d64677d9123b503d6e1db86ef71f9
악동분홍토끼 <pinkrabbit412@daum.net>
2022-03-26 22:33:31 +0900

    Created DelegateAndEvent.cs (WIP)

---
+++ 13. Delegate/DelegateAndEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSharpStudy {
+    public class DelegateAndEvent {
+        public static void Main(string[] args) {
+
+            // Program exit
+            Console.WriteLine("\n\n[?] Press any key to exit... ");
+            Console.ReadKey();
+            return;
+        }
+    }
+}
