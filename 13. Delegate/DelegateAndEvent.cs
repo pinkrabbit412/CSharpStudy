fb1ae9e75bfde85b92400dacd4b9b3cfe1c7bb6d
악동분홍토끼 <pinkrabbit412@daum.net>
2022-04-02 23:53:56 +0900

    Updated DelegateAndEvent.cs (WIP)

--- 13. Delegate/DelegateAndEvent.cs
+++ 13. Delegate/DelegateAndEvent.cs
@@ -1,8 +1,17 @@
 ﻿using System;
 
 namespace CSharpStudy {
+    delegate int DelegateExample1(int a, int b);
+    public class ExampleClass {
+        public static int Plus(int a, int b) { return a + b; }
+        public static int Minus(int a, int b) { return a - b;  }
+    }
     public class DelegateAndEvent {
         public static void Main(string[] args) {
+            // #1. Easy delegate example
+            DelegateExample1 callback;
+            callback = new DelegateExample1(ExampleClass.Plus);
+            Console.WriteLine("[?] callback(5, 7) = {0}", callback(5, 7));
 
             // Program exit
             Console.WriteLine("\n\n[?] Press any key to exit... ");
